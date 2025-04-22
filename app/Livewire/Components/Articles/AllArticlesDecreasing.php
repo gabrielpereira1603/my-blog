171de036c3431451cc4ae6b7470669f4307cea16
gabrielpereira1603 <pereiragabrieldev@gmail,com>
2025-04-22 17:14:44 -0300

    Add AllArticlesDecreasing Livewire component
    Introduced a new Livewire component to display all articles in decreasing order. Included the PHP class and corresponding Blade view to structure the component.

---
+++ app/Livewire/Components/Articles/AllArticlesDecreasing.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Components\Articles;
+
+use Livewire\Component;
+
+class AllArticlesDecreasing extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.articles.all-articles-decreasing');
+    }
+}
